ef6f13c50290c8002e8406ee206c1f8eb62c9f71
minseong0324 <kwan0324@naver.com>
2024-02-02 08:20:28 +0900

    Feat: 내 위치로 돌아가는 버튼 기능 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { NaverMap } from '@/components/NaverMap'
-import PageLayout from '@/components/PageLayout/PageLayout'
-import SearchBar from '@/components/SearchBar'
-import GoToMyLocationButton from '@/components/Button/GoToMyLocationButton/GoToMyLocationButton'
-import GetCafeLocationButton from '@/components/Button/GetCafeLocationButton/GetCafeLocationButton'
-import * as Styled from './style'
+import { NaverMap } from '@/components/NaverMap';
+import PageLayout from '@/components/PageLayout/PageLayout';
+import SearchBar from '@/components/SearchBar';
+import GoToMyLocationButton from '@/components/Button/GoToMyLocationButton/GoToMyLocationButton';
+import GetCafeLocationButton from '@/components/Button/GetCafeLocationButton/GetCafeLocationButton';
+import * as Styled from './style';
+import { useRecoilState } from 'recoil';
+import { myLocationState } from '@/atoms/location';
+import useGeolocation from '@/hooks/useGeolocation';
 
 export default function Home() {
+  const [, setMyLocation] = useRecoilState(myLocationState);
+
+  const { coordinates } = useGeolocation();
+
+
+  const handleMyLocationButtonClicked = () => {
+    setMyLocation({ latitude: coordinates.lat, longitude: coordinates.lng });
+  };
   return (
     <PageLayout>
       <SearchBar />
       <Styled.ButtonContainer>
-        <GoToMyLocationButton />
+        <GoToMyLocationButton onClick={() => handleMyLocationButtonClicked()} />
         <Styled.CenteredButton>
           <GetCafeLocationButton />
         </Styled.CenteredButton>
