955aa4d2af84788b3b15e5766142763ae5f07d1f
minseong0324 <kwan0324@naver.com>
2024-02-02 02:46:44 +0900

    Fix: clientId issue

--- src/components/NaverMap/index.tsx
+++ src/components/NaverMap/index.tsx
@@ -5,7 +5,7 @@ import { NavermapsProvider } from 'react-naver-maps';
 export function NaverMap() {
     return (
         <>
-            <NavermapsProvider ncpClientId={"cb04ozhs17"} >
+            <NavermapsProvider ncpClientId={import.meta.env.VITE_APP_NAVER_CLIENT_ID} >
                 <NaverMapContainer />
             </NavermapsProvider>
         </>
