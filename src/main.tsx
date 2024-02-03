e2e05bd309e0b6f89cd4fb2d08008e5749ab4629
kurtyoon <akakertcobain@naver.com>
2024-02-03 23:19:05 +0900

    Chore: msw 라이브러리 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import {RecoilRoot} from 'recoil';
 import QueryClientContext from './contexts/QueryClientContext';
-import { GlobalStyle } from './style';
+import {GlobalStyle} from './style';
+import {initMocks} from './mocks';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientContext>
-      <RecoilRoot>
-        <App />
-        <GlobalStyle />
-      </RecoilRoot>
-    </QueryClientContext>
-  </React.StrictMode>,
-)
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+  return initMocks();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <QueryClientContext>
+        <RecoilRoot>
+          <App />
+          <GlobalStyle />
+        </RecoilRoot>
+      </QueryClientContext>
+    </React.StrictMode>,
+  );
+});
