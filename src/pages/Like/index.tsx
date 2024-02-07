31e6b776dedd6a15606620d042b98b7fe097a94b
kurtyoon <akakertcobain@naver.com>
2024-02-08 00:56:42 +0900

    Feat: 임시 Filtering Modal 적용

--- src/pages/Like/index.tsx
+++ src/pages/Like/index.tsx
@@ -8,11 +8,15 @@ import {Toggle} from '@/components/Toggle/Toggle';
 import useModal from '@/hooks/useModal';
 import {useEffect, useState} from 'react';
 import {useRecoilState} from 'recoil';
+import SearchBar from '@/components/Search/SearchBar';
+import FilterModal from '@/components/Modal/FilterModal';
+import useInput from '@/hooks/useInput';
+import TempModal from '@/components/Modal/TempModal';
 
 export default function Like() {
   const [likes, setLikes] = useRecoilState(likesState);
   const {isOpen, openModal, closeModal} = useModal();
-  const [searchTerm, setSearchTerm] = useState('');
+  const {value: searchTerm, setValue: setSearchTerm, reset} = useInput();
 
   useEffect(() => {
     fetch('/test/cafes/likes')
@@ -22,21 +26,29 @@ export default function Like() {
       });
   }, []);
 
+  const handleFilterModalOpen = () => {
+    openModal();
+  };
+
   return (
-    <PageLayout>
-      <Styled.ButtonsWrapper>
-        <FilterButton />
-        <Toggle />
-      </Styled.ButtonsWrapper>
-      <LikeSearchBar
-        isOpen={isOpen}
-        openModal={openModal}
-        closeModal={closeModal}
-        placeholder="이름으로 검색하기."
-        setSearchTerm={setSearchTerm}
-        children={''}
-      ></LikeSearchBar>
+    <>
+      <PageLayout>
+        <Styled.ButtonsWrapper>
+          <FilterButton onClick={() => handleFilterModalOpen()} />
+          <Toggle />
+        </Styled.ButtonsWrapper>
+        <SearchBar
+          placeholder={'이름으로 검색하기.'}
+          isOpen={false}
+          openModal={function (): void {}}
+          closeModal={function (): void {}}
+          top={100}
+          reset={reset}
+        />
         <LikeList likes={likes} searchTerm={searchTerm} />
-    </PageLayout>
+      </PageLayout>
+
+      {isOpen && <TempModal isOpen={isOpen} onClose={closeModal} />}
+    </>
   );
 }
