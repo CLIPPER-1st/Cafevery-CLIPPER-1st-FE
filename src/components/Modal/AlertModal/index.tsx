728522c5aef57f2c211f3c1637771e3d703090fd
minseong0324 <kwan0324@naver.com>
2024-02-09 01:43:25 +0900

    Feat: AlertModal setTimeout

--- src/components/Modal/AlertModal/index.tsx
+++ src/components/Modal/AlertModal/index.tsx
@@ -1,8 +1,20 @@
+import React, { useEffect } from 'react';
 import Modal from '@/components/Modal/Modal';
 import theme from '@/theme';
-import * as Styled from './style'
+import * as Styled from './style';
 
 export default function AlertModal({onClose, isOpen, ...props}) {
+
+  useEffect(() => {
+    let timer;
+    if (isOpen) {
+      timer = setTimeout(() => {
+        onClose();
+      }, 800);
+    }
+
+    return () => clearTimeout(timer);
+  }, [isOpen, onClose]);
 
   return (
     <Modal modalTitle={''} isOpen={isOpen} onClose={onClose} modalType={'SmallModal'} modalColor={theme.colors.white} color={theme.colors.darkBrown} fontSize={26} {...props}>
