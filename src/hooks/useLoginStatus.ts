7a63ce3dc8cb5ecc9443619f88c0ec1874cb866e
minseong0324 <kwan0324@naver.com>
2024-02-12 15:35:18 +0900

    Feat:  useLoginStatus 추가

---
+++ src/hooks/useLoginStatus.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+export default function useLoginStatus() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const accessToken = Cookies.get('accessToken');
+        const refreshToken = Cookies.get('refreshToken');
+
+        /**액세스 토큰과 리프레시 토큰이 모두 존재하는지 확인하여 로그인 상태 설정 */ 
+        if (accessToken && refreshToken) {
+        setIsLoggedIn(true);
+        } else {
+        setIsLoggedIn(false);
+        }
+    }, []);
+
+    /**로그아웃 상태 변경을 위한 함수 */ 
+    const logout = () => {
+        Cookies.remove('accessToken');
+        Cookies.remove('refreshToken');
+        setIsLoggedIn(false);
+    };
+
+    return { isLoggedIn, logout };
+}
