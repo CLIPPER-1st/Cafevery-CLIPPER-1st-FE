6d3530007b9535565b5ca7706752742103626137
kurtyoon <akakertcobain@naver.com>
2024-02-16 09:57:31 +0100

    Feat: 설정 페이지 로그아웃 정의

--- src/components/Setting/UserInfo/index.tsx
+++ src/components/Setting/UserInfo/index.tsx
@@ -4,20 +4,17 @@ interface Props {
   isLogin: boolean;
   nickname?: string;
   provider?: string;
+  logout?: () => void;
 }
 
 export default function UserInfo(props: Props) {
-  const handleLogout = () => {
-    console.log('로그아웃');
-  };
-
   return (
     <>
       {props.isLogin ? (
         <Styled.Container>
           <Styled.Label>{props.nickname}</Styled.Label>
           <Styled.Provider>{props.provider}</Styled.Provider>
-          <Styled.Logout onClick={handleLogout}>로그아웃</Styled.Logout>
+          <Styled.Logout onClick={props.logout}>로그아웃</Styled.Logout>
         </Styled.Container>
       ) : (
         <Styled.Label>로그인 후 이용해주세요</Styled.Label>
--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -3,18 +3,20 @@ import PageLayout from '@/components/PageLayout/PageLayout';
 import * as Styled from './style';
 import SettingInfoList from '@/components/Setting/SettingInfoList';
 import UserInfo from '@/components/Setting/UserInfo';
+import {useLoginStatus} from '@/hooks/useLoginStatus';
 
 export default function Setting() {
-  const isLogin = true;
+  const {isLoggedIn, logout} = useLoginStatus();
 
   return (
     <PageLayout>
       <SettingCloseButton />
       <Styled.Wrapper>
         <UserInfo
-          isLogin={isLogin}
+          isLogin={isLoggedIn}
           nickname={'Cafevery'}
           provider={'소셜 로그인 회원'}
+          logout={logout}
         />
       </Styled.Wrapper>
       <Styled.Line />
