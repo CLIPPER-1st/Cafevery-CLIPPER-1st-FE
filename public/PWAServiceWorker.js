28d84efef8063f9bf3a3604eba9fa6c87cc418de
kurtyoon <akakertcobain@naver.com>
2024-02-16 10:31:15 +0100

    Fix: PWA Service worker cache state 변경

--- public/PWAServiceWorker.js
+++ public/PWAServiceWorker.js
@@ -1,44 +1,66 @@
 // Define Cache data
 const CACHE_NAME = 'pwa-cache-v1';
-const offlinePage = 'offline.html';
-const urlsToCache = ['/', '/manifest.json', '/index.html', offlinePage];
+const urlsToCache = ['/', '/manifest.json', '/index.html', '/offline.html'];
 
 // Install Event
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] installed');
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] caching all: app shell and content');
-      return cache.addAll();
+      return cache.addAll(urlsToCache).then(() => {
+        return self.skipWaiting();
+      });
     }),
   );
 });
 
 // Activate Event
-self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] Activate');
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[Service Worker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        }),
-      );
-    }),
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          }),
+        );
+      })
+      .then(() => {
+        return self.clients.claim();
+      }),
   );
 });
 
 // Fetch Event
-self.addEventListener('fetch', (e) => {
-  console.log('[Service Worker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return (
-        response || fetch(e.request).catch(() => caches.match(offlinePage))
-      );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request)
+        .then((response) => {
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== 'basic'
+          ) {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        })
+        .catch(() => {
+          return caches.match('/offline.html');
+        });
     }),
   );
 });
