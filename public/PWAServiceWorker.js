ed7ff16eb1c3ebfe27b953e2a4b5b1fead79c25e
minseong0324 <kwan0324@naver.com>
2024-02-23 08:33:59 +0900

    Chore: PWA version up

--- public/PWAServiceWorker.js
+++ public/PWAServiceWorker.js
@@ -1,5 +1,5 @@
 // Define Cache data
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'pwa-cache-v2';
 const urlsToCache = ['/', '/manifest.json', '/index.html', '/offline.html'];
 
 // Install Event
