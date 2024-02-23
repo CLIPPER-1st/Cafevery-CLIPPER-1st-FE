7131cd7fb332ed91b3490fc1ef2850d85ba177f7
kurtyoon <akakertcobain@naver.com>
2024-02-23 22:34:03 +0000

    Feat: PWA Service Worker 변경

--- public/PWAServiceWorker.js
+++ public/PWAServiceWorker.js
@@ -1,66 +1,70 @@
-// Define Cache data
-const CACHE_NAME = 'pwa-cache-v2';
-const urlsToCache = ['/', '/manifest.json', '/index.html', '/offline.html'];
+// // Define Cache data
+// const CACHE_NAME = 'pwa-cache-v2';
+// const urlsToCache = ['/', '/manifest.json', '/index.html', '/offline.html'];
 
-// Install Event
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache).then(() => {
-        return self.skipWaiting();
-      });
-    }),
-  );
-});
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/PWAServiceWorker.js');
+// }
 
-// Activate Event
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (cacheName !== CACHE_NAME) {
-              return caches.delete(cacheName);
-            }
-          }),
-        );
-      })
-      .then(() => {
-        return self.clients.claim();
-      }),
-  );
-});
+// // Install Event
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll(urlsToCache).then(() => {
+//         return self.skipWaiting();
+//       });
+//     }),
+//   );
+// });
 
-// Fetch Event
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
+// // Activate Event
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then((cacheNames) => {
+//         return Promise.all(
+//           cacheNames.map((cacheName) => {
+//             if (cacheName !== CACHE_NAME) {
+//               return caches.delete(cacheName);
+//             }
+//           }),
+//         );
+//       })
+//       .then(() => {
+//         return self.clients.claim();
+//       }),
+//   );
+// });
 
-      return fetch(event.request)
-        .then((response) => {
-          if (
-            !response ||
-            response.status !== 200 ||
-            response.type !== 'basic'
-          ) {
-            return response;
-          }
+// // Fetch Event
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       if (response) {
+//         return response;
+//       }
 
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseToCache);
-          });
+//       return fetch(event.request)
+//         .then((response) => {
+//           if (
+//             !response ||
+//             response.status !== 200 ||
+//             response.type !== 'basic'
+//           ) {
+//             return response;
+//           }
 
-          return response;
-        })
-        .catch(() => {
-          return caches.match('/offline.html');
-        });
-    }),
-  );
-});
+//           var responseToCache = response.clone();
+//           caches.open(CACHE_NAME).then((cache) => {
+//             cache.put(event.request, responseToCache);
+//           });
+
+//           return response;
+//         })
+//         .catch(() => {
+//           return caches.match('/offline.html');
+//         });
+//     }),
+//   );
+// });
