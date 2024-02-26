1f9459f6bb0642485420ccb80e5f94ab9ba68c4f
minseong0324 <kwan0324@naver.com>
2024-02-27 01:12:42 +0900

    Fix: 무한 새로고침 이슈

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,8 @@ import App from './App';
 import {RecoilRoot} from 'recoil';
 import {GlobalStyle} from './style';
 import {initMocks} from './mocks';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
-import {register} from './serviceWorkerRegistration';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 async function enableMocking() {
   if (!import.meta.env.VITE_APP_NODE_ENV) {
@@ -17,23 +16,22 @@ async function enableMocking() {
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 0,
-    },
+      queries: {
+          refetchOnWindowFocus: false,
+          retry: 0,
+      },
   },
 });
 
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <App />
-        <GlobalStyle />
-      </RecoilRoot>
-      <ReactQueryDevtools />
+        <RecoilRoot>
+          <App />
+          <GlobalStyle />
+        </RecoilRoot>
+        <ReactQueryDevtools />
     </QueryClientProvider>,
+
   );
 });
-
-register();
