27e6c5925bb1e6d071a31b1ce9c82dee47b24bfb
minseong0324 <kwan0324@naver.com>
2024-02-27 15:48:40 +0900

    Fix: api 엔드포인트

--- src/apis/cafeList.ts
+++ src/apis/cafeList.ts
@@ -1,6 +1,6 @@
 import { instance } from './axios';
 
 export const fetchCafes = async (centerLatitude: number, centerLongitude: number) => {
-    const response = await instance.get(`api/v1/cafes/?centerLatitude=${centerLatitude}&centerLongitude=${centerLongitude}`);
+    const response = await instance.get(`api/v1/cafes?centerLatitude=${centerLatitude}&centerLongitude=${centerLongitude}`);
     return response;
 };
