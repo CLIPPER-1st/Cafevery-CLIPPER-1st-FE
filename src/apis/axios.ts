cfd5ce4a81e5a9271fbb0ba2ac175b8aae375483
minseong0324 <kwan0324@naver.com>
2024-02-27 17:12:10 +0900

    Fix : Cookies.get

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -2,20 +2,20 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import useSetTokens from '../hooks/useSetTokens';
 
-const getAccessTokenFromCookies = () => Cookies.get('access_token');
-const getRefreshTokenFromCookies = () => Cookies.get('refresh_token');
+const accessToken = Cookies.get('access_token');
+const refreshToken = Cookies.get('refresh_token');
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVER_URL, //TODO: 서버 URL
   //baseURL:"",
   headers: {
-    Authorization: `Bearer ${getAccessTokenFromCookies()}`,
+    Authorization: `Bearer ${accessToken}`,
   },
 });
 
 instance.interceptors.request.use(
   config => {
-    config.headers.Authorization = `Bearer ${getAccessTokenFromCookies()}`;
+    config.headers.Authorization = `Bearer ${accessToken}`;
     return config;
   },
   error => Promise.reject(error)
@@ -28,13 +28,12 @@ instance.interceptors.response.use(
 
     if (error.response && error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
-      const refreshToken = getRefreshTokenFromCookies();
       if (!refreshToken) {
         throw new Error('토큰 없음');
       }
       
         await sendRefreshToken(refreshToken);
-        originalRequest.headers['Authorization'] = `Bearer ${getAccessTokenFromCookies()}`;
+        originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
         return instance(originalRequest);
     }
 
