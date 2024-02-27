c520114d2adf25d1c3ea5504a80344c05ec1384d
minseong0324 <kwan0324@naver.com>
2024-02-27 15:33:06 +0900

    Fix: api 엔드포인트

--- src/apis/registerFavoritePlace.ts
+++ src/apis/registerFavoritePlace.ts
@@ -4,7 +4,7 @@ import { instance } from './axios';
 
 export const registerFavoritePlace = async (body: PostRegisterFavoritePlaceRequest) => {
     try {
-        const response = await instance.post<PostRegisterFavoritePlaceRequest>(`/users/location`, {
+        const response = await instance.post<PostRegisterFavoritePlaceRequest>(`api/v1/users/location`, {
             name: body.name,
             latitude: body.latitude,
             longitude: body.longitude,
