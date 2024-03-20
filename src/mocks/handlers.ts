aff6923e5b64c3141af4c27b9c1868bacdaacec7
minseong0324 <kwan0324@naver.com>
2024-03-21 02:50:17 +0900

    Style: 폴더명 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,10 @@
-import { cafeInfoHandler } from './apis/cafeInfo';
-import { cafesHandler } from './apis/cafes';
+import { cafeInfoHandler } from './apis/getCafeInfo';
+import { cafesHandler } from './apis/getCafes';
 import { getSettingUserInfoHandler } from './apis/getSettingUserInfo';
-import {likeHandler} from './apis/like';
+import {likeHandler} from './apis/getLike';
 import { patchNicknameHandler } from './apis/patchNickname';
 import { putLikeHandler } from './apis/putLike';
-import { userInfoHandler } from './apis/userInfo';
+import { userInfoHandler } from './apis/getUserInfo';
 
 export const handlers = [
     ...cafeInfoHandler, 
